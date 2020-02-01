6b148b4b27582c7532353077abd4c8654356e5f6
Konstantinos Paschalidis <konstantinospaschalidis@validitylabs.org>
2020-02-01 20:36:10 +0100

    Update intro text.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Initial App.
+         GitLab mirroring is working!
         </p>
       </header>
     </div>
